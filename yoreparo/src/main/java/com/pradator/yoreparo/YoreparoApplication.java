7168cbbeac7d7148d7965ac259fc1373a8121632
Carlos Prada <carlosprada95@gmail.com>
2020-11-14 11:17:10 -0500

    creacion del repositorio para yoreparo

---
+++ yoreparo/src/main/java/com/pradator/yoreparo/YoreparoApplication.java
@@ -0,0 +1,13 @@
+package com.pradator.yoreparo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoreparoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoreparoApplication.class, args);
+	}
+
+}
