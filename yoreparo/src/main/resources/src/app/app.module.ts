99bf47a4c8a1f75d15b69aef3d393cae59d3fb8c
ever ospino <everospino97@gmail.com>
2020-11-19 01:42:59 -0500

    navbar añadido

--- yoreparo/src/main/resources/src/app/app.module.ts
+++ yoreparo/src/main/resources/src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './components/app/app-routing.module';
 import { AppComponent } from './components/app/app.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ yoreparo/src/main/resources/src/app/components/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { HomeComponent } from './../../components/home/home.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'cotizar', component: HomeComponent},
+  {path: 'garantia', component: HomeComponent},
+  {path: 'ayuda', component: HomeComponent},
+  {path: 'sesion', component: HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
